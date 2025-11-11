9c67a0d70db7c136d15f08484e6902c455382dae
Nate <nkoele413@gmail.com>
2025-11-10 17:32:18 -0700

    command_help fix

--- command_help.go
+++ command_help.go
@@ -1,13 +1,14 @@
 package main
+
 import "fmt"
 
-func commandHelp(cfg * config) error {
+func commandHelp(cfg *config) error {
 	fmt.Println()
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
 	for _, cmd := range getCommands() {
-		fmt.Println("%s: %s\n", cmd.name, cmd.description)
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
 	fmt.Println()
 	return nil
