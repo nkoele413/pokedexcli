0537b00b62f26f401232466a7c48f75ac7ab8a36
Nate <nkoele413@gmail.com>
2025-11-11 17:34:59 -0700

    FIxed maps and help

--- command_help.go
+++ command_help.go
@@ -1,40 +1,15 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
-func commandMapf(cfg *config, args ...string) error {
-	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
-	if err != nil {
-		return err
+func commandHelp(cfg *config, args ...string) error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
-
-	cfg.nextLocationsURL = locationsResp.Next
-	cfg.prevLocationsURL = locationsResp.Previous
-
-	for _, loc := range locationsResp.Results {
-		fmt.Println(loc.Name)
-	}
-	return nil
-}
-
-func commandMapb(cfg *config, args ...string) error {
-	if cfg.prevLocationsURL == nil {
-		return errors.New("you're on the first page")
-	}
-
-	locationResp, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
-	if err != nil {
-		return err
-	}
-
-	cfg.nextLocationsURL = locationResp.Next
-	cfg.prevLocationsURL = locationResp.Previous
-
-	for _, loc := range locationResp.Results {
-		fmt.Println(loc.Name)
-	}
+	fmt.Println()
 	return nil
 }
